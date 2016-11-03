e85489a474df88f557fe5ca38f2efbec03ec347a
shiva444 <madiraju92@gmail.com>
2016-11-02 22:59:04 -0400

    Add files via upload

--- LoginController+handlers.swift
+++ LoginController+handlers.swift
@@ -7,29 +7,103 @@
 //
 
 import UIKit
+import Firebase
 
 extension LoginController: UIImagePickerControllerDelegate,
     UINavigationControllerDelegate {
+    
+    
+    func handleRegister() {
+        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
+            print("Form is not valid")
+            return
+        }
+        FIRAuth.auth()?.createUser(withEmail: email, password: password,
+                                   completion: { (user: FIRUser?, error) in
+                                    
+                                    if error != nil {
+                                        print(error)
+                                        return
+                                    }
+                                    
+                                    
+                                    guard let uid = user?.uid else {
+                                        return
+                                    }
+                                    //successfully authenticated user
+                                    
+                let storageRef = FIRStorage.storage().reference().child("myImage.png")
+                            
+                if let uploadData = UIImagePNGRepresentation(self.profileImageView.image!) {
+                                        
+                                    
+                storageRef.put(uploadData, metadata: nil, completion:
+                        { (metadata, error) in
+                            if error != nil {
+                            print(error)
+                                return
+                            }
+                            print(metadata)
+                    })
+            }
+                    
+                                    
+                    let ref = FIRDatabase.database().reference(fromURL: "https://chatapp-9cd47.firebaseio.com/" )
+                    let usersReference = ref.child("user").child(uid)
+                                    
+                        let values = ["name": name, "email": email]
+                                    
+                            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+                                if err != nil {
+                                    print (err)
+                                        return
+                                        }
+                                        
+                                        self.dismiss(animated: true, completion: nil)
+                                    })
+                                    
+        })
+    }
+    
+    
+    private func registerUserIntoDatabase() {
+        
+        
+        
+        
+        
+        
+    }
+    
     func handleSelectProfileImageView() {
     let picker = UIImagePickerController()
         
         picker.delegate = self
+        picker.allowsEditing = true
+        
         present(picker, animated: true, completion: nil)
     }
     
     
-  private func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+   func imagePickerController(_ _picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
     
-    if let originalImage =
-        info["UIImagePickerControllerOriginalImage"] {
-        print(originalImage.size)
+    var selectedImageFromPicker: UIImage?
+    
+    if let edittedImage = info["UIImagePickerControllerEdittedImage"]
+    as? UIImage {
+        selectedImageFromPicker = edittedImage
+    } else if let originalImage =
+        info["UIImagePickerControllerOriginalImage"] as? UIImage {
+        
+        selectedImageFromPicker = originalImage
+           }
+    if let selectedImage = selectedImageFromPicker {
+    profileImageView.image = selectedImage
+        
     }
-    
+    dismiss(animated: true, completion: nil)
+}
 
-    
-    }
-    
-    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         print("canceled picker")
         
